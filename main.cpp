1f543f9aa816ed2e568eb6b3e68890da407e8c44
tijeets21 <145161295+tijeets21@users.noreply.github.com>
2025-04-10 20:31:49 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file main.cpp
+ * @brief Entry point for phone orientation detection system
+ * @author Your Name
+ * @date 2025
+ */
+
+#include "../include/UserInterface.h"
+
+/**
+ * @brief Program entry point
+ * @return Exit status (0 for success)
+ */
+int main() {
+    UserInterface ui;
+    ui.run();
+    return 0;
+}
